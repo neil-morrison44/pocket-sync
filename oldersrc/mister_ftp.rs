928ede1575cd722148f2094ba1224d9a46f25e65
Neil Morrison <email@neil.today>
2022-11-20 21:59:52 +0000

    Tidy & add tauri workflows

--- oldersrc/mister_ftp.rs
+++
@@ -1,9 +0,0 @@
-pub fn logged_in_ftp(
-    host: &str,
-    user: &str,
-    password: &str,
-) -> Result<suppaftp::FtpStream, suppaftp::FtpError> {
-    let mut ftp_stream = suppaftp::FtpStream::connect(format!("{host}:21"))?;
-    ftp_stream.login(user, password)?;
-    Ok(ftp_stream)
-}
