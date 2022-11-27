27bb4a8c15f6aa72068d0318a206dba209dae13a
Neil Morrison <email@neil.today>
2022-11-27 16:26:26 +0000

    Improve the image editor layout

--- src/components/platforms/info/imageEditor/index.tsx
+++ src/components/platforms/info/imageEditor/index.tsx
@@ -190,10 +190,6 @@ export const ImageEditor = ({
     invalidateFS(Date.now())
 
     onClose()
-
-    // console.log({ buffer })
-
-    // setTestImage(renderBinImage(buffer, width, height, false))
   }, [width, height])
 
   const moveStamp = useCallback(
@@ -202,108 +198,122 @@ export const ImageEditor = ({
         const currentIndex = stamps.findIndex((s) => s === stamp)
         const newStamps = stamps.filter((s) => s !== stamp)
         newStamps.splice(currentIndex + move, 0, stamp)
-
         return newStamps
       })
+
+      setSelectedStampIndex((i) => i + move)
     },
     [setImageStamps, setSelectedStampIndex]
   )
 
   return (
-    <Modal>
-      <div className="image-editor__close-button" onClick={onClose}>
-        X
-      </div>
+    <Modal className="image-editor">
       <canvas
         className="image-editor__preview-canvas"
         width={width}
         height={height}
         ref={canvasRef}
       ></canvas>
+      <div className="image-editor__stamps-etc">
+        <div className="image-editor__stamps">
+          {imageStamps.map((stamp, index) => {
+            const selected = index === selectedStampIndex
 
-      <div className="image-editor__stamps">
-        {imageStamps.map((stamp, index) => {
-          const selected = index === selectedStampIndex
-
-          return (
-            <div
-              className={`image-editor__stamp image-editor__stamp--${
-                selected ? "selected" : "not-selected"
-              }`}
-              key={index}
-              onClick={() => setSelectedStampIndex(index)}
-            >
-              <input type="checkbox" checked={selected} readOnly />
-              <img
-                className="image-editor__stamp-image"
-                src={stamp.src}
-                onClick={() => {
-                  setSelectedStampIndex(index)
-                }}
-              ></img>
-
-              <div className="image-editor__stamp-controls">
-                {"Scale"}
-                <input
-                  type="range"
-                  max={5}
-                  step={0.001}
-                  min={0.001}
-                  value={stamp.scale}
-                  onChange={({ target }) =>
-                    updateImageStampValues(stamp, {
-                      scale: parseFloat(target.value),
-                    })
-                  }
-                ></input>
-                {"Rotate"}
-                <input
-                  type="range"
-                  max={180}
-                  min={-180}
-                  value={stamp.rotate}
-                  onChange={({ target }) =>
-                    updateImageStampValues(stamp, {
-                      rotate: parseInt(target.value),
-                    })
-                  }
-                ></input>
-                <button
-                  onClick={() =>
-                    updateImageStampValues(stamp, {
-                      y: 0,
-                      x: 0,
-                      scale: 1,
-                      rotate: 0,
-                    })
-                  }
-                >
-                  Reset
-                </button>
-              </div>
-
-              <div className="image-editor__stamp-controls">
-                {index !== 0 && (
-                  <button onClick={() => moveStamp(stamp, -1)}>Up</button>
-                )}
-                {index !== imageStamps.length - 1 && (
-                  <button onClick={() => moveStamp(stamp, 1)}>Down</button>
-                )}
-              </div>
+            return (
               <div
-                className="image-editor__stamp-controls"
-                onClick={() => {
-                  setImageStamps((stamps) => stamps.filter((s) => s !== stamp))
-                }}
+                className={`image-editor__stamp image-editor__stamp--${
+                  selected ? "selected" : "not-selected"
+                }`}
+                key={index}
+                onMouseDown={() => setSelectedStampIndex(index)}
               >
-                <button>Remove</button>
+                <img
+                  className="image-editor__stamp-image"
+                  src={stamp.src}
+                  onClick={() => {
+                    setSelectedStampIndex(index)
+                  }}
+                ></img>
+
+                <div className="image-editor__stamp-controls">
+                  {"Scale"}
+                  <input
+                    type="range"
+                    className="image-editor__range"
+                    max={5}
+                    step={0.001}
+                    min={0.001}
+                    value={stamp.scale}
+                    onChange={({ target }) =>
+                      updateImageStampValues(stamp, {
+                        scale: parseFloat(target.value),
+                      })
+                    }
+                  ></input>
+                  {"Rotate"}
+                  <input
+                    type="range"
+                    className="image-editor__range"
+                    max={180}
+                    min={-180}
+                    value={stamp.rotate}
+                    onChange={({ target }) =>
+                      updateImageStampValues(stamp, {
+                        rotate: parseInt(target.value),
+                      })
+                    }
+                  ></input>
+                </div>
+
+                <div className="image-editor__stamp-controls">
+                  <button
+                    onClick={() => {
+                      setImageStamps((stamps) =>
+                        stamps.filter((s) => s !== stamp)
+                      )
+                    }}
+                  >
+                    Remove
+                  </button>
+                  <button
+                    onClick={() =>
+                      updateImageStampValues(stamp, {
+                        y: 0,
+                        x: 0,
+                        scale: 1,
+                        rotate: 0,
+                      })
+                    }
+                  >
+                    Reset
+                  </button>
+                </div>
+                <div className="image-editor__stamp-controls">
+                  {index !== 0 && (
+                    <button
+                      className="image-editor__layer-move-button image-editor__layer-move-button--up"
+                      onClick={() => moveStamp(stamp, -1)}
+                    ></button>
+                  )}
+                  {index !== imageStamps.length - 1 && (
+                    <button
+                      className="image-editor__layer-move-button image-editor__layer-move-button--down"
+                      onClick={() => moveStamp(stamp, 1)}
+                    ></button>
+                  )}
+                </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
+        <button onClick={addNewImage}>Import Image</button>
       </div>
-      <button onClick={addNewImage}>Import Image</button>
-      <button onClick={saveBinary}>Save</button>
+      <div className="image-editor__bottom-buttons">
+        <button onClick={saveBinary}>Save</button>
+        <button className="image-editor__close-button" onClick={onClose}>
+          Cancel
+        </button>
+      </div>
     </Modal>
   )
 }
