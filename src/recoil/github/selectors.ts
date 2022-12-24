acb6507727ffc2b77833c74572a16de868febc74
Neil Morrison <email@neil.today>
2022-12-24 03:21:20 +0000

    Better error for rate limiting & window remembers position

--- src/recoil/github/selectors.ts
+++ src/recoil/github/selectors.ts
@@ -15,6 +15,20 @@ export const GithubReleasesSelectorFamily = selectorFamily<
         `https://api.github.com/repos/${owner}/${repo}/releases`
       )
 
+      const remainingRateLimit = parseInt(
+        response.headers.get("x-ratelimit-remaining") || "60"
+      )
+
+      if (remainingRateLimit < 1) {
+        const timeTillReset = parseInt(
+          response.headers.get("x-ratelimit-reset") || "0"
+        )
+        const resetDate = new Date(timeTillReset * 1000)
+        throw new Error(
+          `GitHub rate limit reached, retry after ${resetDate.toLocaleString()}`
+        )
+      }
+
       return (await response.json()) as GithubRelease[]
     },
 })
