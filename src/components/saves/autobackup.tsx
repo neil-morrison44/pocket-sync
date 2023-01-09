12f2c9f3394d8a2417152a3f9cdeb22e2851b803
Neil Morrison <email@neil.today>
2023-01-09 00:13:15 +0000

    Fixes the pixels at the edges of platform images

--- src/components/saves/autobackup.tsx
+++ src/components/saves/autobackup.tsx
@@ -15,7 +15,6 @@ export const AutoBackup = () => {
     ;(async () => {
       for (const save of saves) {
         const exists = await invokeFileExists(save.backup_location)
-        console.log({exists})
         if (!exists) continue
         await invokeBackupSaves(
           allSaves,
