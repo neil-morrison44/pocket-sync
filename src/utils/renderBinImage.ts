a28550e46bed0a19beacd9dc0f34948b51ce1b5d
Neil Morrison <email@neil.today>
2023-01-09 00:30:14 +0000

    Fixes the pixels at the edges of platform images (#55)
    * Fixes the pixels at the edges of platform images
    
    * Also do a version bump

--- src/utils/renderBinImage.ts
+++ src/utils/renderBinImage.ts
@@ -8,21 +8,17 @@ export const renderBinImage = (
   const context = canvas.getContext("2d")
   if (!context) throw new Error("Failed to get canvas context")
 
-  canvas.width = width
+  // not sure why this should be 1px wider than it should be
+  // but it fixes an off-by-one error so it'll do
+  canvas.width = width + 1
   canvas.height = height
 
-  const halfWidth = Math.ceil(width / 2)
-  const halfHeight = Math.ceil(height / 2)
-
-  context.translate(halfWidth, halfHeight)
-  context.rotate(Math.PI / 2)
-  context.translate(-halfHeight, -halfWidth)
-
   let index = 0
-  for (let y = 0; y < width; y++) {
-    for (let x = 0; x < height; x++) {
+  for (let x = width; x >= 0; x--) {
+    for (let y = 0; y < height; y++) {
       const value = image[index]
       const brightness = invert ? 255 - value : value
+
       context.fillStyle = `rgb(${brightness}, ${brightness}, ${brightness})`
       context.fillRect(x, y, 1, 1)
 
