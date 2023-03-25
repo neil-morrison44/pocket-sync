dddf11946e1e1e86d61e52989f904ba749e589c3
Neil Morrison <email@neil.today>
2023-03-25 14:24:13 +0000

    Improved inventory updating (#91)
    * Improved inventory updating
    
    * oops, set this back to 10 mins
    
    * Add some cache busting stuff & get the refresh button refreshing
    
    * Add a cache bust to the newsfeed too (won't be a disk cache but there could be a cache somewhere)

--- src/recoil/inventory/atoms.ts
+++ src/recoil/inventory/atoms.ts
@@ -1,12 +1,37 @@
 import { atom } from "recoil"
 import { InventoryJSON } from "../../types"
 
+const INTERVAL_MINS = 10
+
+const INVENTORY_API =
+  "https://openfpga-cores-inventory.github.io/analogue-pocket/api/v2/cores.json"
+
 export const coreInventoryAtom = atom<InventoryJSON>({
   key: "coreInventoryAtom",
   default: (async () => {
-    const response = await fetch(
-      "https://openfpga-cores-inventory.github.io/analogue-pocket/api/v2/cores.json"
-    )
+    const response = await fetch(INVENTORY_API + `?cache_bust=${Date.now()}`)
     return (await response.json()) as InventoryJSON
   })(),
+  effects: [
+    ({ onSet, setSelf }) => {
+      onSet(async (_a, _b, isReset) => {
+        // isReset should work here but it doesn't seem to
+        // so instead do this for any outside set
+        const response = await fetch(
+          INVENTORY_API + `?cache_bust=${Date.now()}`
+        )
+        const newJson: InventoryJSON = await response.json()
+        setSelf(newJson)
+      })
+    },
+    ({ setSelf }) => {
+      setInterval(async () => {
+        const response = await fetch(
+          INVENTORY_API + `?cache_bust=${Date.now()}`
+        )
+        const newJson: InventoryJSON = await response.json()
+        setSelf(newJson)
+      }, INTERVAL_MINS * 60 * 1000)
+    },
+  ],
 })
