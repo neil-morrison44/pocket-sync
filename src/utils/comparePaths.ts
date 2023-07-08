b397c14407aedd662285b71160ad22c82f575e24
Neil Morrison <email@neil.today>
2023-07-08 17:32:20 +0100

    more tidy

--- src/utils/comparePaths.ts
+++ src/utils/comparePaths.ts
@@ -1,3 +1,3 @@
 export const comparePaths = (a: string[], b: string[]): boolean => {
-    return a.length === b.length &&  a.every((value, index) => value === b[index])
+  return a.length === b.length && a.every((value, index) => value === b[index])
 }
