d491589d2b99b1e4adaa1565fdfdcec8f91caf2b
Neil Morrison <email@neil.today>
2023-11-06 21:15:03 +0000

    Fix modals on wide windows

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react"
-
+import { ReactNode, useEffect, useState } from "react"
+import { WebviewWindow } from "@tauri-apps/api/window"
 import "./index.css"
 
 type ModalProps = {
@@ -8,8 +8,28 @@ type ModalProps = {
 }
 
 export const Modal = ({ children, className }: ModalProps) => {
+  const [wrapperHeight, setWrapperHeight] = useState(0)
+
+  useEffect(() => {
+    const mainWindow = WebviewWindow.getByLabel("main")
+    if (!mainWindow) return
+
+    mainWindow.innerSize().then(({ height }) => {
+      setWrapperHeight(height)
+    })
+
+    const unlisten = mainWindow.onResized(({ payload }) => {
+      setWrapperHeight(payload.height)
+    })
+    return () => {
+      unlisten.then((l) => l())
+    }
+  }, [])
+
+  console.log({ wrapperHeight })
+
   return (
-    <div className="modal__wrapper">
+    <div className="modal__wrapper" style={{ height: `${wrapperHeight}px` }}>
       <div className={`modal ${className || ""}`}>{children}</div>
     </div>
   )
