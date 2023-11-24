21e8b5c17283e5b9e61bd8c7c001dc28936047e8
Remy Sharp <remy@remysharp.com>
2023-11-24 16:53:55 +0000

    Update polyfills.ts - add hasOwn
    Add support for older MacOS versions

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -8,4 +8,18 @@ export const installPolyfills = () => {
       }
     }
   }
+
+  if (!Object.hasOwn) {
+    Object.defineProperty(Object, "hasOwn", {
+      value: function (object, property) {
+        if (object == null) {
+          throw new TypeError("Cannot convert undefined or null to object")
+        }
+        return Object.prototype.hasOwnProperty.call(Object(object), property)
+      },
+      configurable: true,
+      enumerable: false,
+      writable: true
+    })
+  }
 }
