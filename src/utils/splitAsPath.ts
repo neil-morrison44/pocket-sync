55bd0489816b5edcaa1dfbe9de715b92b8f048b4
Neil Morrison <email@neil.today>
2023-12-30 22:09:20 +0000

    tidy

--- src/utils/splitAsPath.ts
+++ src/utils/splitAsPath.ts
@@ -1,15 +1,12 @@
-import {platform, Platform} from "@tauri-apps/api/os"
-
-let PLATFORM: null | Platform  = null
-platform().then((p) => PLATFORM = p)
+// let PLATFORM: null | Platform = null
+// platform().then((p) => (PLATFORM = p))
 
 export const splitAsPath = (filePath: string): string[] => {
-
-
-    return filePath.split(/\\|\//g).filter(Boolean)
-    if (PLATFORM === "win32"){
-        return filePath.split("\\").filter(Boolean)
-    } else {
-        return filePath.split("/").filter(Boolean)
-    }
+  return filePath.split(/\\|\//g).filter(Boolean)
+  // Need to support both for some things since hardcoded in-app paths use /
+  // if (PLATFORM === "win32"){
+  //     return filePath.split("\\").filter(Boolean)
+  // } else {
+  //     return filePath.split("/").filter(Boolean)
+  // }
 }
