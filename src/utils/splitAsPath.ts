303d0e9dc65f4b70922a3c303bf1a5f0f077bf0d
Neil Morrison <email@neil.today>
2023-12-30 22:08:01 +0000

    A couple of filepath related fixes for windows

--- src/utils/splitAsPath.ts
+++ src/utils/splitAsPath.ts
@@ -4,6 +4,9 @@ let PLATFORM: null | Platform  = null
 platform().then((p) => PLATFORM = p)
 
 export const splitAsPath = (filePath: string): string[] => {
+
+
+    return filePath.split(/\\|\//g).filter(Boolean)
     if (PLATFORM === "win32"){
         return filePath.split("\\").filter(Boolean)
     } else {
