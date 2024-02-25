361bb0df1c54105d44ae5be4d834c436dd4b3e04
Neil Morrison <2095051+neil-morrison44@users.noreply.github.com>
2024-02-25 13:46:28 +0000

    Fixes issue with the md5 data slots (#254)

--- src/utils/filterFiles.ts
+++ src/utils/filterFiles.ts
@@ -1,7 +1,12 @@
+import { splitAsPath } from "./splitAsPath"
+
 export const filterKnownBadFiles = (paths: string[]): string[] =>
   paths.filter((p) => {
     if (p.startsWith("__MACOSX/") || p.endsWith(".DS_Store")) return false
     if (p.endsWith(".keep") || p.endsWith(".gitkeep")) return false
+    if (p.endsWith(".md")) return false
+    if (splitAsPath(p).length === 1 && p.endsWith(".txt")) return false
+    if (p.endsWith(".mra")) return false
 
     return true
   })
