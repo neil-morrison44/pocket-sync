62bf810a5a2def3d8f24fec1673fabb50a7a8e82
Neil Morrison <email@neil.today>
2024-05-12 23:46:24 +0100

    Adds locks to the file system to fix issues where write tasks cause read tasks to be called a lot

---
+++ src-tauri/src/file_locks/mod.rs
@@ -0,0 +1,50 @@
+use futures_locks::{RwLockReadGuard, RwLockWriteGuard};
+use std::sync::Arc;
+use std::{collections::HashMap, path::PathBuf};
+use tokio::sync::RwLock;
+
+#[derive(Default, Clone)]
+pub struct ArcLock(Arc<RwLock<()>>);
+
+impl ArcLock {
+    pub fn is_readable(&self) -> bool {
+        self.0.try_read().is_ok()
+    }
+
+    pub fn read(&self) -> impl futures::Future<Output = tokio::sync::RwLockReadGuard<'_, ()>> {
+        self.0.read()
+    }
+
+    pub fn write(&self) -> impl futures::Future<Output = tokio::sync::RwLockWriteGuard<'_, ()>> {
+        self.0.write()
+    }
+}
+
+#[derive(Default)]
+pub struct FileLocks(RwLock<HashMap<PathBuf, ArcLock>>);
+
+impl FileLocks {
+    pub async fn find_lock_for(&self, path: &PathBuf) -> ArcLock {
+        {
+            let file_locks = self.0.read().await;
+
+            // work the way up ancestors, return the first one that currently can't be read
+            if let Some(blocked_lock) = path
+                .ancestors()
+                .filter_map(|p| file_locks.get(&p.to_path_buf()))
+                .find(|lock| !lock.is_readable())
+            {
+                return blocked_lock.clone();
+            }
+        }
+
+        // failing that, create a lock for this path & add it to the hash
+        {
+            let mut file_locks = self.0.write().await;
+
+            let new_lock = ArcLock::default();
+            file_locks.insert(path.clone(), new_lock.clone());
+            return new_lock;
+        }
+    }
+}
--- src-tauri/src/util/mod.rs
+++ src-tauri/src/util/mod.rs
@@ -2,7 +2,10 @@ use anyhow::Result;
 use bytes::BytesMut;
 use futures::StreamExt as _;
 use reqwest::Response;
-use std::{path::PathBuf, time::SystemTime};
+use std::{
+    path::{Path, PathBuf},
+    time::SystemTime,
+};
 
 pub async fn get_mtime_timestamp(path: &PathBuf) -> Result<u64> {
     let metadata = tokio::fs::metadata(path).await.and_then(|m| m.modified())?;
@@ -33,4 +36,44 @@ pub async fn progress_download(
         on_progress(total_size, downloaded);
     }
     Ok(file)
+}
+
+pub fn find_common_path<'a>(paths: impl IntoIterator<Item = &'a PathBuf>) -> Option<PathBuf> {
+    let mut path_iter = paths.into_iter();
+    let mut result = path_iter.next()?.to_path_buf();
+    for path in path_iter {
+        if let Some(r) = common_path(&result, &path) {
+            result = r;
+        } else {
+            return None;
+        }
+    }
+    Some(result.to_path_buf())
+}
+
+fn common_path<P, Q>(one: P, two: Q) -> Option<PathBuf>
+where
+    P: AsRef<Path>,
+    Q: AsRef<Path>,
+{
+    let one = one.as_ref();
+    let two = two.as_ref();
+    let one = one.components();
+    let two = two.components();
+    let mut final_path = PathBuf::new();
+    let mut found = false;
+    let paths = one.zip(two);
+    for (l, r) in paths {
+        if l == r {
+            final_path.push(l.as_os_str());
+            found = true;
+        } else {
+            break;
+        }
+    }
+    if found {
+        Some(final_path)
+    } else {
+        None
+    }
 }
