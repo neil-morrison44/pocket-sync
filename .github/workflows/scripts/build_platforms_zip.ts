4b7102f2bf2edb262a57a52d90af0fa846656f21
Neil Morrison <email@neil.today>
2024-05-19 20:18:58 +0100

    Changes the image pack stuff to read the variants from the releases

--- .github/workflows/scripts/build_platforms_zip.ts
+++ .github/workflows/scripts/build_platforms_zip.ts
@@ -4,87 +4,103 @@ import { Octokit } from "@octokit/rest"
 
 export const buildPlatformZip = async ({ github }: { github: Octokit }) => {
   const url =
-    "https://raw.githubusercontent.com/mattpannella/pocket-updater-utility/main/image_packs.json"
+    "https://raw.githubusercontent.com/neil-morrison44/pocket-sync/main/image_packs.json"
   const response = await github.request(url)
   const packs = JSON.parse(response.data) as {
     owner: string
     repository: string
-    variant?: string
   }[]
 
   const multiZip = new zip.ZipWriter(new zip.BlobWriter("application/zip"), {
     bufferedWrite: true,
   })
 
+  let imagePackOutput: {
+    owner: string
+    repository: string
+    variant: string
+  }[] = []
+
   for (const pack of packs) {
-    const latestRelease = await (
+    const latestRelease = (await (
       await fetch(
         `https://api.github.com/repos/${pack.owner}/${pack.repository}/releases/latest`
       )
-    ).json()
+    ).json()) as {
+      assets?: {
+        name: string
+        browser_download_url: string
+      }[]
+    }
     if (!latestRelease?.assets) {
       console.error(latestRelease)
       continue
     }
-    const downloadURL = latestRelease.assets.find(({ name }) => {
-      if (!name.endsWith(".zip")) return false
-      if (pack.variant)
-        return (
-          name.endsWith(`${pack.variant}.zip`) ||
-          name.startsWith(`${pack.variant}_`)
-        )
-      return true
-    })
-    if (!downloadURL) continue
 
-    const zipResponse = await github.request(downloadURL.browser_download_url)
-    const zipBlob = new Blob([zipResponse.data], {
-      type: "application/zip",
-    })
+    const variants = latestRelease.assets.filter(({ name }) =>
+      name.endsWith(".zip")
+    )
 
-    const abortController = new AbortController()
-    const entries = await new zip.ZipReader(new zip.BlobReader(zipBlob), {
-      signal: abortController.signal,
-    }).getEntries({})
+    for (const variant of variants) {
+      const name = variant.name.replace(".zip", "")
 
-    const packPath = `${pack.owner}__${pack.repository}__${
-      pack.variant || "default"
-    }`
+      imagePackOutput.push({
+        ...pack,
+        variant: name,
+      })
 
-    for (const entry of entries.filter((entry) =>
-      entry.filename.endsWith(".json")
-    )) {
-      if (!entry.getData) return
-      const [file, ..._] = entry.filename.split("/").reverse()
-      const filename = `${packPath}/Platforms/${file}`
-      const dataBlob = await entry.getData(new zip.BlobWriter(), {})
-      try {
-        await multiZip.add(filename, new zip.BlobReader(dataBlob))
-      } catch (err) {
-        console.log(filename)
-        console.error(err)
+      const downloadURL = variant.browser_download_url
+      if (!downloadURL) continue
+
+      const zipResponse = await github.request(downloadURL)
+      const zipBlob = new Blob([zipResponse.data], {
+        type: "application/zip",
+      })
+
+      const abortController = new AbortController()
+      const entries = await new zip.ZipReader(new zip.BlobReader(zipBlob), {
+        signal: abortController.signal,
+      }).getEntries({})
+
+      const packPath = `${pack.owner}__${pack.repository}__${name}`
+
+      for (const entry of entries.filter((entry) =>
+        entry.filename.endsWith(".json")
+      )) {
+        if (!entry.getData) return
+        const [file, ..._] = entry.filename.split("/").reverse()
+        const filename = `${packPath}/Platforms/${file}`
+        const dataBlob = await entry.getData(new zip.BlobWriter(), {})
+        try {
+          await multiZip.add(filename, new zip.BlobReader(dataBlob))
+        } catch (err) {
+          console.log(filename)
+          console.error(err)
+        }
       }
-    }
 
-    for (const entry of entries.filter((entry) =>
-      entry.filename.endsWith(".bin")
-    )) {
-      if (!entry.getData) return
-      const [file, ..._] = entry.filename.split("/").reverse()
-      const filename = `${packPath}/Platforms/_images/${file}`
-      const dataBlob = await entry.getData(new zip.BlobWriter(), {})
-      try {
-        await multiZip.add(filename, new zip.BlobReader(dataBlob))
-      } catch (err) {
-        console.log(filename)
-        console.error(err)
+      for (const entry of entries.filter((entry) =>
+        entry.filename.endsWith(".bin")
+      )) {
+        if (!entry.getData) return
+        const [file, ..._] = entry.filename.split("/").reverse()
+        const filename = `${packPath}/Platforms/_images/${file}`
+        const dataBlob = await entry.getData(new zip.BlobWriter(), {})
+        try {
+          await multiZip.add(filename, new zip.BlobReader(dataBlob))
+        } catch (err) {
+          console.log(filename)
+          console.error(err)
+        }
       }
     }
   }
 
   const blob = await multiZip.close()
   const buffer = Buffer.from(await blob.arrayBuffer())
+
   await writeFile("platforms.zip", buffer)
+  await writeFile("image_packs.json", JSON.stringify(imagePackOutput, null, 2))
 }
 
 // await buildPlatformZip({ github: new Octokit() })
