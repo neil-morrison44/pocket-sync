fe71bcb719b771455e7bd2d2908d32cce1cbd83f
Neil Morrison <2095051+neil-morrison44@users.noreply.github.com>
2024-07-27 19:09:06 +0100

    Adds support for changing the JOTEGO analogizer config (#303)

---
+++ src/components/cores/info/jtanalogizer/index.tsx
@@ -0,0 +1,80 @@
+import { useRecoilState, useRecoilValue } from "recoil"
+import { DataJSONSelectorFamily } from "../../../../recoil/selectors"
+import { Suspense, useMemo } from "react"
+import {
+  JT_ANALOGIZER_SNAC_OPTIONS,
+  JT_ANALOGIZER_VIDEO_OPTIONS,
+  JTCRTConfigSelector,
+} from "../../../../recoil/cores/selectors"
+import { useTranslation } from "react-i18next"
+import { Details } from "../../../shared/details"
+import { AnalogizerIcon } from "../../icons/AnalogizerIcon"
+import { SupportsBubble } from "../supportsBubble"
+
+import "./index.css"
+
+type JTAnalogizerSettingsProps = {
+  coreName: string
+}
+
+export const JTAnalogizerSettings = ({
+  coreName,
+}: JTAnalogizerSettingsProps) => {
+  const coreData = useRecoilValue(DataJSONSelectorFamily(coreName))
+  const hasCRTConfig = useMemo(
+    () =>
+      coreData.data.data_slots.find(
+        ({ filename }) => filename === "crtcfg.bin"
+      ) !== undefined,
+    [coreData]
+  )
+
+  if (!hasCRTConfig) return null
+
+  return (
+    <Suspense fallback={<div style={{ height: "230px" }}></div>}>
+      <JTAnalogizerSettingsInner />
+    </Suspense>
+  )
+}
+
+const JTAnalogizerSettingsInner = () => {
+  const { t } = useTranslation("core_info")
+  const [crtConfig, setCrtConfig] = useRecoilState(JTCRTConfigSelector)
+
+  return (
+    <Details
+      title={t("jt_analogizer_config.title")}
+      renderIcon={() => <AnalogizerIcon />}
+    >
+      <h4 className="jt-analogizer__options-title">
+        {t("jt_analogizer_config.video")}
+      </h4>
+      <div className="jt-analogizer__options">
+        {JT_ANALOGIZER_VIDEO_OPTIONS.map(([_, name]) => (
+          <SupportsBubble
+            supports={crtConfig.video === name}
+            key={name}
+            onClick={() => setCrtConfig({ ...crtConfig, video: name })}
+          >
+            {name}
+          </SupportsBubble>
+        ))}
+      </div>
+      <h4 className="jt-analogizer__options-title">
+        {t("jt_analogizer_config.snac")}
+      </h4>
+      <div className="jt-analogizer__options">
+        {JT_ANALOGIZER_SNAC_OPTIONS.map(([_, name]) => (
+          <SupportsBubble
+            supports={crtConfig.snac === name}
+            key={name}
+            onClick={() => setCrtConfig({ ...crtConfig, snac: name })}
+          >
+            {name}
+          </SupportsBubble>
+        ))}
+      </div>
+    </Details>
+  )
+}
