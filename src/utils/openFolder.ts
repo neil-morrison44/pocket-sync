d16577396ec10b4c9513cc77df52ec8ac53b22b1
Neil Morrison <neil-morrison44@users.noreply.github.com>
2025-09-23 22:36:56 +0100

    fix windows opener issue

--- src/utils/openFolder.ts
+++ src/utils/openFolder.ts
@@ -4,5 +4,6 @@ import { openPath } from "@tauri-apps/plugin-opener"
 export const openFolder = async (folderPath: string) => {
   const sep = path.sep()
   const platformPath = folderPath.replaceAll("/", sep)
+  console.log(platformPath)
   openPath(platformPath)
 }
